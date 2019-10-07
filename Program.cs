51cf41aaad1d8e66e9089fd3bd28e550ee4a7222
kellycoles <kellycoles1@gmail.com>
2019-10-06 21:29:04 -0500

    part 1 done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace dictionary
 {
@@ -6,7 +7,38 @@ namespace dictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        
+          /*
+    Create a dictionary with key value pairs to
+    represent words (key) and its definition (value)
+*/
+Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>();
+
+// Add several more words and their definitions
+wordsAndDefinitions.Add("Awesome", "the feeling of students when they are learning C#");
+wordsAndDefinitions.Add("cast", "the process of making a variable temporarily behave as if it were a different type.");
+wordsAndDefinitions.Add("Emulator", "a device that simulates the operation of some type of hardware.");
+wordsAndDefinitions.Add("FORTRAN", "a high level programming lsnguage introduced in the 1950's");
+
+/*
+    Use square brackets to get the definition of two of the
+    words and then output them to the console
+*/
+ Console.WriteLine(wordsAndDefinitions["cast"]);
+  Console.WriteLine(wordsAndDefinitions["FORTRAN"]);
+/*
+    Below, loop over the wordsAndDefinitions dictionary to get the following output:
+        The definition of (WORD) is (DEFINITION)
+        The definition of (WORD) is (DEFINITION)
+        The definition of (WORD) is (DEFINITION)
+*/
+Console.WriteLine("---------------------------------------------------------------------------------");
+foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
+{
+    Console.WriteLine($"The definition of {word.Key} is{word.Value}");
+}
+
+
         }
     }
 }
